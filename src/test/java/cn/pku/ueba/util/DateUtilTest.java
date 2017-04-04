5a38f411ec05e92e7305d463f79e22dbd10ce1f5
xixy10 <xixy10@foxmail.com>
2017-04-04 14:31:53 +0800

    增加了对DateUtil的测试

---
+++ src/test/java/cn/pku/ueba/util/DateUtilTest.java
@@ -0,0 +1,76 @@
+/**
+ * @author xixy10@foxmail.com
+ * @version V0.1 2017年4月4日 下午2:09:51
+ */
+package cn.pku.ueba.util;
+
+import static org.junit.Assert.fail;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+/**
+ * DateUtil的测试文件
+ * 
+ * @author xixy10@foxmail.com
+ */
+public class DateUtilTest {
+
+	private static String esDate = DateUtil.getESDate();
+
+	/**
+	 * Test method for {@link cn.pku.ueba.util.DateUtil#getESDate()}.
+	 */
+	@Test
+	public void testGetESDate() {
+		String date = DateUtil.getESDate();
+		if (date == null) {
+			fail("getESDate failed");
+		}
+	}
+
+	/**
+	 * Test method for
+	 * {@link cn.pku.ueba.util.DateUtil#toESDate(java.util.Date)}.
+	 */
+	@Test
+	public void testToESDate() {
+		String date = DateUtil.toESDate(new Date());
+		if (date == null)
+			fail("toESDate failed");
+	}
+
+	/**
+	 * Test method for {@link cn.pku.ueba.util.DateUtil#getLastDayESDate(int)}.
+	 */
+	@Test
+	public void testGetLastDayESDate() {
+		String date = DateUtil.getLastDayESDate(5);
+		if (date == null)
+			fail("GetLastDayESDate");
+	}
+
+	/**
+	 * Test method for {@link cn.pku.ueba.util.DateUtil#getYesterdayDate(int)}.
+	 */
+	@Test
+	public void testGetYesterdayDate() {
+		Date date = DateUtil.getYesterdayDate(2);
+		if (date == null)
+			fail("getYesterdayDate failed");
+
+	}
+
+	/**
+	 * Test method for
+	 * {@link cn.pku.ueba.util.DateUtil#getDateFromESDate(java.lang.String)}.
+	 */
+	@Test
+	public void testGetDateFromESDate() {
+		Date date = DateUtil.getDateFromESDate(esDate);
+		if (!esDate.equals(DateUtil.toESDate(date)))
+			fail("getDateFromESDate failed");
+	}
+
+}
