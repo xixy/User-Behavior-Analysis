c658f138dc14ab9479748bdca4b0701cdcc3893f
xixy10 <xixy10@foxmail.com>
2017-04-04 14:30:21 +0800

    增加了特征生成器的runnable模块

---
+++ src/main/java/cn/pku/ueba/service/FeatureVectorGenerator.java
@@ -0,0 +1,23 @@
+/**
+ * @author xixy10@foxmail.com
+ * @version V0.1 2017年4月4日 下午1:45:32
+ */
+package cn.pku.ueba.service;
+
+/**
+ * 用来生成特征向量的Runnable接口的类
+ * 
+ * @author xixy10@foxmail.com
+ */
+public class FeatureVectorGenerator implements Runnable {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+
+	}
+
+}
